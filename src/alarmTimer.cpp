1b4002cd2d4431312a247d8d8105d046bee0ad8a
DonBoxGit <donbox@yandex.ru>
2023-05-10 22:54:34 +0300

    Added timeDataCheck function

--- src/alarmTimer.cpp
+++ src/alarmTimer.cpp
@@ -1,6 +1,7 @@
-#include "alarmTime.h"
+#include "alarmTimer.h"
 
 MicroDS3231 *pRTC = new MicroDS3231(RTC_I2C_ADDR);
+AlarmTime  alarm_1, alarm_2;
 
 uint8_t readRegisterDS3231() {
   Wire.begin();
@@ -18,4 +19,16 @@ void writeRegisterDS3231(uint8_t val) {
   Wire.write(CONTROL_REGISTER);
   Wire.write(val);
   Wire.endTransmission();
+}
+
+void timeDataCheck(AlarmTime *pAlarmTime) {
+  if (pAlarmTime->hours > 23 || pAlarmTime->minutes > 59 ||
+      pAlarmTime->seconds > 59) {
+    pAlarmTime->hours = 0;
+    pAlarmTime->minutes = 0;
+    pAlarmTime->seconds = 0;
+    /* If wrong data then to reset and to put in EEPROM */
+    if (pAlarmTime == ALARM_1_ADDR) EEPROM.put(ALARM_1_ADDR, pAlarmTime);
+    else EEPROM.put(ALARM_2_ADDR, pAlarmTime);
+  }
 }
